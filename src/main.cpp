69130146ec679c58a522ce716850eca7e835d8e9
Eloi0424 <beichu0424@qq.com>
2024-07-19 16:06:01 +0800

    7.19

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <ctime>
-#include <utility>
+#include <bits/stdc++.h>
 #include "SimpleGraph.h"
+
+#include <unistd.h>
 
 using std::cin;
 using std::cout;
@@ -12,6 +10,7 @@ using std::endl;
 void Welcome();
 void ReadGraph();
 void InitGraph();
+void ForceDirectLayoutAlgo();
 
 const double kPi = 3.14159265358979323;
 const double kRepel = 0.001;
@@ -30,11 +29,10 @@ int main()
     Welcome();
 
     ReadGraph();
-
+    InitGraphVisualizer(graph);
     InitGraph();
 
-    InitGraphVisualizer(graph);
-    DrawGraph(graph);
+    ForceDirectLayoutAlgo();
 
     return 0;
 }
@@ -51,19 +49,21 @@ void Welcome()
 
 void ReadGraph()
 {
+    std::ifstream inFile;
+
     cout << "Input you graph name" << endl;
     char graphName[100];
     cin >> graphName;
-    freopen(graphName, "r", stdin);
+    inFile.open(graphName);
     size_t u, v;
-    cin >> Node_n;
-    while (cin >> u >> v)
+    inFile >> Node_n;
+    while (inFile >> u >> v)
     {
         graph.edges.push_back(Edge{u, v});
         Edge_m++;
     }
+    inFile.close();
     cout << "Read graph compelete" << endl;
-    fclose(stdin);
 }
 
 void InitGraph()
@@ -72,6 +72,9 @@ void InitGraph()
     {
         graph.nodes.push_back(Node{cos(2 * kPi * i / Node_n), sin(2 * kPi * i / Node_n)});
     }
+
+    DrawGraph(graph);
+
     cout << "Init graph node position compelete" << endl;
 }
 
@@ -79,19 +82,40 @@ void ForceDirectLayoutAlgo()
 {
     cout << "Input the seconds you want algo run" << endl;
     cin >> timeLimit;
+    cout << timeLimit << endl;
     time_t startTime = time(NULL);
     double elapsedTime;
     while (1)
     {
+        usleep(500000);
         elapsedTime = difftime(time(NULL), startTime);
         if (elapsedTime > timeLimit)
             break;
         double fRepel, fAttract;
+        Node start, end;
+        double theta;
         for (int i = 0; i < Edge_m; i++)
         {
-            fRepel = kRepel / sqrt(pow(graph.edges[i].start - graph.edges[i].end, 2) + pow(graph.nodes[i].y - graph.nodes[j].y, 2));
-            fAttract = kAttract * (pow(graph.nodes[i].x - graph.nodes[j].x, 2) + pow(graph.nodes[i].y - graph.nodes[j].y, 2));
-            forces[i].
+            start = graph.nodes[graph.edges[i].start];
+            end = graph.nodes[graph.edges[i].end];
+            theta = atan2(end.y - start.y, end.x - start.x);
+            fRepel = kRepel / sqrt(pow(end.x - start.x, 2) + pow(end.y - start.y, 2));
+            fAttract = kAttract * (pow(end.x - start.x, 2) + pow(end.y - start.y, 2));
+            forces[graph.edges[i].start].first -= fRepel * cos(theta);
+            forces[graph.edges[i].start].second -= fRepel * sin(theta);
+            forces[graph.edges[i].end].first += fRepel * cos(theta);
+            forces[graph.edges[i].end].second += fRepel * sin(theta);
+
+            forces[graph.edges[i].start].first += fAttract * cos(theta);
+            forces[graph.edges[i].start].second += fAttract * sin(theta);
+            forces[graph.edges[i].end].first -= fAttract * cos(theta);
+            forces[graph.edges[i].end].second -= fAttract * sin(theta);
         }
+        for (int i = 0; i < Node_n; i++)
+        {
+            graph.nodes[i].x += forces[i].first;
+            graph.nodes[i].y += forces[i].second;
+        }
+        DrawGraph(graph);
     }
 }
